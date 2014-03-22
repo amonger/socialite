5f9d8d4ded90127cf0abfce2179ad87509d7722c
Alan Monger <Alan@Alan-PC.(none)>
2014-03-22 21:29:36 +0000

    changes the facade to a factory

--- Socialite/Service/ServiceFactory.php
+++ Socialite/Service/ServiceFactory.php
@@ -5,9 +5,9 @@
     use Guzzle\Plugin\Oauth\OauthPlugin;
     use Guzzle\Service\Client;
 
-    class TwitterFacade
+    class ServiceFactory
     {
-        public static function get($oauth = array())
+        public static function twitter($oauth = array())
         {
             return new Twitter(new Client(), new OauthPlugin($oauth));
         }
