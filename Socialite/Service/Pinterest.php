37964abede33c57d56ac32fdff93b20eec8d7f8d
Alan Monger <haveacigaro@gmail.com>
2014-04-19 12:14:59 +0100

    Renames files

---
+++ Socialite/Service/Pinterest.php
@@ -0,0 +1,22 @@
+<?php
+    namespace Socialite\Service;
+/**
+ * Created by PhpStorm.
+ * User: alan
+ * Date: 3/22/14
+ * Time: 2:02 PM
+ */
+
+
+    class Pinterest implements ServiceInterface
+    {
+        public function __construct ()
+        {
+
+        }
+
+        public function post (GuzzleClient $client)
+        {
+
+        }
+    }
